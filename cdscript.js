220225bf4dd9b06bfdc38eb2a72bd6a729939b3d
M1zushi <usersama2006@gmail.com>
2021-04-20 17:50:55 +0400

    Refactored JS (pain...also still not functional)

--- cdscript.js
+++ cdscript.js
@@ -1,25 +1,40 @@
-let sp1 = 00;
-let sp2 = 00;
-let sp3 = 00;
+var sp1 = 0;
+var sp2 = 0;
+var sp3 = 0;
+var slog1 = 'Placeholder'
+var slog2 = 'Placeholder'
+var slog3 = 'Placeholder'
 
 function stopwatch() {
+      sp1++;
       if (sp1 < 10) {
-        sp1++;
-        document.querySelector('p1').innerHTML = ('0' + sp3 + ':0' + sp2 + ':0' + sp1);
+        slog1 = (':0' + sp1);
       }
       else if (sp1 >= 10) {
-        sp1++;
-        document.querySelector('p1').innerHTML = ('0' + sp3 + ':0' + sp2 + ':' + sp1);
+        slog1 = (':' + sp1);
       }
-      else if (sp1 >= 60) {
-        var sp1 = 0;
-        if (sp2 >= 10) {
-          sp2++;
-          document.querySelector('p1').innerHTML = ('0' + sp3 + ':' + sp2 + ':0' + sp1);
-        }
-        else if (sp2 < 10) {
-          sp2++;
-          document.querySelector('p1').innerHTML = ('0' + sp3 + ':0' + sp2 + ':0' + sp1);
-        }
+      else if (sp1 > 60) {
+        sp1 = 0;
+        sp2++;
       }
+
+      if (sp2 < 10) {
+        slog2 = (':0' + sp2);
+      }
+      else if (sp2 >= 10) {
+        slog2 = (':' + sp2);
+      }
+      else if (sp2 >= 60) {
+        sp2 = 0;
+        sp3++;
+      }
+
+      if (sp3 < 10) {
+        slog3 = ('0' + sp3);
+      }
+      else if (sp3 >= 10) {
+        slog3 = (sp3);
+      }
+
+      document.querySelector('p1').innerHTML = (slog3 + slog2 + slog1);
 }
