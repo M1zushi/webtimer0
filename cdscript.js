640e698cf8f3065f29a8c1de313245dba12c3e25
M1zushi <usersama2006@gmail.com>
2021-04-21 17:51:01 +0400

    I broke it.

--- cdscript.js
+++ cdscript.js
@@ -4,37 +4,44 @@ var sp3 = 0;
 var slog1 = 'Placeholder'
 var slog2 = 'Placeholder'
 var slog3 = 'Placeholder'
+// var loop = 0;
+// var useless = 5;
 
-function stopwatch() {
-      sp1++;
-      if (sp1 < 10) {
-        slog1 = (':0' + sp1);
-      }
-      else if (sp1 >= 10) {
-        slog1 = (':' + sp1);
-      }
-      else if (sp1 > 60) {
-        sp1 = 0;
-        sp2++;
-      }
+// while (loop == 0) {
+  function stopwatch() {
+        sp1++;
+        if (sp1 < 10) {
+          slog1 = (':0' + sp1);
+        }
+        else if (sp1 >= 10) {
+          slog1 = (':' + sp1);
+        }
+        else if (sp1 > 60) {
+          var sp1 = 0;
+          sp2++;
+        }
 
-      if (sp2 < 10) {
-        slog2 = (':0' + sp2);
-      }
-      else if (sp2 >= 10) {
-        slog2 = (':' + sp2);
-      }
-      else if (sp2 >= 60) {
-        sp2 = 0;
-        sp3++;
-      }
+        if (sp2 < 10) {
+          slog2 = (':0' + sp2);
+        }
+        else if (sp2 >= 10) {
+          slog2 = (':' + sp2);
+        }
+        else if (sp2 >= 60) {
+          sp2 = 0;
+          sp3++;
+        }
 
-      if (sp3 < 10) {
-        slog3 = ('0' + sp3);
-      }
-      else if (sp3 >= 10) {
-        slog3 = (sp3);
-      }
+        if (sp3 < 10) {
+          slog3 = ('0' + sp3);
+        }
+        else if (sp3 >= 10) {
+          slog3 = (sp3);
+        }
 
-      document.querySelector('p1').innerHTML = (slog3 + slog2 + slog1);
-}
+        document.querySelector('p1').innerHTML = (slog3 + slog2 + slog1);
+  }
+//   if (loop < useless) {
+//     setTimeout(stopwatch, 1000);
+//   }
+// }
