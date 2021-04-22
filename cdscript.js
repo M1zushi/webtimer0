a5fff508aca257b72462342d2aec9e8f6b7deab7
M1zushi <usersama2006@gmail.com>
2021-04-22 21:03:11 +0400

    Conditional Waterfall Debug #1

--- cdscript.js
+++ cdscript.js
@@ -8,39 +8,104 @@ var slog3 = 'Placeholder'
 // var useless = 5;
 
 // while (loop == 0) {
-  function stopwatch() {
-        sp1++;
-        if (sp1 < 10) {
-          slog1 = (':0' + sp1);
-        }
-        else if (sp1 >= 10) {
-          slog1 = (':' + sp1);
-        }
-        else if (sp1 > 60) {
-          var sp1 = 0;
-          sp2++;
-        }
-
+function stopwatch() {
+      sp1++;
+      if (sp1 < 10) {
+        slog1 = (':0' + sp1);
         if (sp2 < 10) {
           slog2 = (':0' + sp2);
+          if (sp3 < 10) {
+            slog3 = ('0' + sp3);
+          }
+          else if (sp3 >= 10) {
+            slog3 = (sp3);
+          }
         }
         else if (sp2 >= 10) {
           slog2 = (':' + sp2);
+          if (sp3 < 10) {
+            slog3 = ('0' + sp3);
+          }
+          else if (sp3 >= 10) {
+            slog3 = (sp3);
+          }
         }
         else if (sp2 >= 60) {
           sp2 = 0;
           sp3++;
+          if (sp3 < 10) {
+            slog3 = ('0' + sp3);
+          }
+          else if (sp3 >= 10) {
+            slog3 = (sp3);
+          }
         }
-
-        if (sp3 < 10) {
-          slog3 = ('0' + sp3);
+      }
+      else if (sp1 >= 10) {
+        slog1 = (':' + sp1);
+        if (sp2 < 10) {
+          slog2 = (':0' + sp2);
+          if (sp3 < 10) {
+            slog3 = ('0' + sp3);
+          }
+          else if (sp3 >= 10) {
+            slog3 = (sp3);
+          }
         }
-        else if (sp3 >= 10) {
-          slog3 = (sp3);
+        else if (sp2 >= 10) {
+          slog2 = (':' + sp2);
+          if (sp3 < 10) {
+            slog3 = ('0' + sp3);
+          }
+          else if (sp3 >= 10) {
+            slog3 = (sp3);
+          }
         }
-
-        document.querySelector('p1').innerHTML = (slog3 + slog2 + slog1);
-  }
+        else if (sp2 >= 60) {
+          sp2 = 0;
+          sp3++;
+          if (sp3 < 10) {
+            slog3 = ('0' + sp3);
+          }
+          else if (sp3 >= 10) {
+            slog3 = (sp3);
+          }
+        }
+      }
+      else if (sp1 > 60) {
+        var sp1 = 0;
+        sp2++;
+        if (sp2 < 10) {
+          slog2 = (':0' + sp2);
+          if (sp3 < 10) {
+            slog3 = ('0' + sp3);
+          }
+          else if (sp3 >= 10) {
+            slog3 = (sp3);
+          }
+        }
+        else if (sp2 >= 10) {
+          slog2 = (':' + sp2);
+          if (sp3 < 10) {
+            slog3 = ('0' + sp3);
+          }
+          else if (sp3 >= 10) {
+            slog3 = (sp3);
+          }
+        }
+        else if (sp2 >= 60) {
+          sp2 = 0;
+          sp3++;
+          if (sp3 < 10) {
+            slog3 = ('0' + sp3);
+          }
+          else if (sp3 >= 10) {
+            slog3 = (sp3);
+          }
+        }
+      }
+      document.querySelector('p1').innerHTML = (slog3 + slog2 + slog1);
+}
 //   if (loop < useless) {
 //     setTimeout(stopwatch, 1000);
 //   }
