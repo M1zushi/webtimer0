0ff8c0e8184a86d4f383e2e29e5d759be7cb2d4a
M1zushi <usersama2006@gmail.com>
2021-05-03 21:44:32 +0400

    Debugging #1

--- cdscript.js
+++ cdscript.js
@@ -1,77 +1,77 @@
 var sp1 = 0;
 var sp2 = 0;
 var sp3 = 0;
-var slog1 = 'Placeholder'
-var slog2 = 'Placeholder'
-var slog3 = 'Placeholder'
+var slog1 = 'Placeholder';
+var slog2 = 'Placeholder';
+var slog3 = 'Placeholder';
 
 function stopwatch() {
       sp1++;
       if (sp1 < 10) {
-        slog1 = (':0' + sp1);
+        var slog1 = (':0' + sp1);
         if (sp2 < 10) {
-          slog2 = (':0' + sp2);
+          var slog2 = (':0' + sp2);
           if (sp3 < 10) {
-            slog3 = ('0' + sp3);
+            var slog3 = ('0' + sp3);
           }
           else if (sp3 >= 10) {
-            slog3 = (sp3);
+            var slog3 = (sp3);
           }
         }
 
         else if (sp2 >= 10) {
-          slog2 = (':' + sp2);
+          var slog2 = (':' + sp2);
           if (sp3 < 10) {
-            slog3 = ('0' + sp3);
+            var slog3 = ('0' + sp3);
           }
           else if (sp3 >= 10) {
-            slog3 = (sp3);
+            var slog3 = (sp3);
           }
         }
 
         else if (sp2 >= 60) {
-          sp2 = 0;
+          var sp2 = 0;
           sp3++;
           if (sp3 < 10) {
-            slog3 = ('0' + sp3);
+            var slog3 = ('0' + sp3);
           }
           else if (sp3 >= 10) {
-            slog3 = (sp3);
+            var slog3 = (sp3);
           }
         }
       }
 
 
       else if (sp1 >= 10) {
-        slog1 = (':' + sp1);
+        var slog1 = (':' + sp1);
         if (sp2 < 10) {
-          slog2 = (':0' + sp2);
+          var slog2 = (':0' + sp2);
           if (sp3 < 10) {
-            slog3 = ('0' + sp3);
+            var slog3 = ('0' + sp3);
           }
           else if (sp3 >= 10) {
-            slog3 = (sp3);
+            var slog3 = (sp3);
           }
         }
 
         else if (sp2 >= 10) {
-          slog2 = (':' + sp2);
+          var slog2 = (':' + sp2);
           if (sp3 < 10) {
-            slog3 = ('0' + sp3);
+            var slog3 = ('0' + sp3);
           }
           else if (sp3 >= 10) {
-            slog3 = (sp3);
+            var slog3 = (sp3);
           }
         }
 
         else if (sp2 >= 60) {
-          sp2 = 0;
+          var sp2 = 0;
           sp3++;
           if (sp3 < 10) {
-            slog3 = ('0' + sp3);
+            var slog3 = ('0' + sp3);
           }
           else if (sp3 >= 10) {
-            slog3 = (sp3);
+            var slog3 = (sp3);
           }
         }
       }
@@ -81,33 +81,33 @@ function stopwatch() {
         var sp1 = 0;
         sp2++;
         if (sp2 < 10) {
-          slog2 = (':0' + sp2);
+          var slog2 = (':0' + sp2);
           if (sp3 < 10) {
-            slog3 = ('0' + sp3);
+            var slog3 = ('0' + sp3);
           }
           else if (sp3 >= 10) {
-            slog3 = (sp3);
+            var slog3 = (sp3);
           }
         }
 
         else if (sp2 >= 10) {
-          slog2 = (':' + sp2);
+          var slog2 = (':' + sp2);
           if (sp3 < 10) {
-            slog3 = ('0' + sp3);
+            var slog3 = ('0' + sp3);
           }
           else if (sp3 >= 10) {
-            slog3 = (sp3);
+            var slog3 = (sp3);
           }
         }
-        
+
         else if (sp2 >= 60) {
-          sp2 = 0;
+          var sp2 = 0;
           sp3++;
           if (sp3 < 10) {
-            slog3 = ('0' + sp3);
+            var slog3 = ('0' + sp3);
           }
           else if (sp3 >= 10) {
-            slog3 = (sp3);
+            var slog3 = (sp3);
           }
         }
       }
