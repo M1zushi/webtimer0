abe3cbdb983ae99e925075a214020c60b029fa51
M1zushi <usersama2006@gmail.com>
2021-05-04 22:06:10 +0400

    All commentted, absolutely unfunctional and unoptimal, README for more

--- cdscript.js
+++ cdscript.js
@@ -1,115 +1,116 @@
-var sp1 = 0;
-var sp2 = 0;
-var sp3 = 0;
-var slog1 = 'Placeholder';
-var slog2 = 'Placeholder';
-var slog3 = 'Placeholder';
+// Yeah this was a set failure
 
-function stopwatch() {
-      sp1++;
-      if (sp1 < 10) {
-        var slog1 = (':0' + sp1);
-        if (sp2 < 10) {
-          var slog2 = (':0' + sp2);
-          if (sp3 < 10) {
-            var slog3 = ('0' + sp3);
-          }
-          else if (sp3 >= 10) {
-            var slog3 = (sp3);
-          }
-        }
-
-        else if (sp2 >= 10) {
-          var slog2 = (':' + sp2);
-          if (sp3 < 10) {
-            var slog3 = ('0' + sp3);
-          }
-          else if (sp3 >= 10) {
-            var slog3 = (sp3);
-          }
-        }
-
-        else if (sp2 >= 60) {
-          var sp2 = 0;
-          sp3++;
-          if (sp3 < 10) {
-            var slog3 = ('0' + sp3);
-          }
-          else if (sp3 >= 10) {
-            var slog3 = (sp3);
-          }
-        }
-      }
-
-
-      else if (sp1 >= 10) {
-        var slog1 = (':' + sp1);
-        if (sp2 < 10) {
-          var slog2 = (':0' + sp2);
-          if (sp3 < 10) {
-            var slog3 = ('0' + sp3);
-          }
-          else if (sp3 >= 10) {
-            var slog3 = (sp3);
-          }
-        }
-
-        else if (sp2 >= 10) {
-          var slog2 = (':' + sp2);
-          if (sp3 < 10) {
-            var slog3 = ('0' + sp3);
-          }
-          else if (sp3 >= 10) {
-            var slog3 = (sp3);
-          }
-        }
-
-        else if (sp2 >= 60) {
-          var sp2 = 0;
-          sp3++;
-          if (sp3 < 10) {
-            var slog3 = ('0' + sp3);
-          }
-          else if (sp3 >= 10) {
-            var slog3 = (sp3);
-          }
-        }
-      }
-
-
-      else if (sp1 > 60) {
-        var sp1 = 0;
-        sp2++;
-        if (sp2 < 10) {
-          var slog2 = (':0' + sp2);
-          if (sp3 < 10) {
-            var slog3 = ('0' + sp3);
-          }
-          else if (sp3 >= 10) {
-            var slog3 = (sp3);
-          }
-        }
-
-        else if (sp2 >= 10) {
-          var slog2 = (':' + sp2);
-          if (sp3 < 10) {
-            var slog3 = ('0' + sp3);
-          }
-          else if (sp3 >= 10) {
-            var slog3 = (sp3);
-          }
-        }
-
-        else if (sp2 >= 60) {
-          var sp2 = 0;
-          sp3++;
-          if (sp3 < 10) {
-            var slog3 = ('0' + sp3);
-          }
-          else if (sp3 >= 10) {
-            var slog3 = (sp3);
-          }
-        }
-      }
-      document.querySelector('p1').innerHTML = (slog3 + slog2 + slog1);
-}
+// var sp1 = 0;
+// var sp2 = 0;
+// var sp3 = 0;
+// var slog1 = 'Placeholder';
+// var slog2 = 'Placeholder';
+// var slog3 = 'Placeholder';
+//
+// function stopwatch() {
+//       sp1++;
+//       if (sp1 < 10) {
+//         slog1 = (':0' + sp1);
+//         if (sp2 < 10) {
+//           slog2 = (':0' + sp2);
+//           if (sp3 < 10) {
+//             slog3 = ('0' + sp3);
+//           }
+//           else if (sp3 >= 10) {
+//             slog3 = (sp3);
+//           }
+//         }
+//
+//         else if (sp2 >= 10) {
+//           slog2 = (':' + sp2);
+//           if (sp3 < 10) {
+//             slog3 = ('0' + sp3);
+//           }
+//           else if (sp3 >= 10) {
+//             slog3 = (sp3);
+//           }
+//         }
+//
+//         else if (sp2 >= 60) {
+//           sp2 = 0;
+//           sp3++;
+//           if (sp3 < 10) {
+//             slog3 = ('0' + sp3);
+//           }
+//           else if (sp3 >= 10) {
+//             slog3 = (sp3);
+//           }
+//         }
+//       }
+//
+//       else if (sp1 >= 10) {
+//         slog1 = (':' + sp1);
+//         if (sp2 < 10) {
+//           slog2 = (':0' + sp2);
+//           if (sp3 < 10) {
+//             slog3 = ('0' + sp3);
+//           }
+//           else if (sp3 >= 10) {
+//             slog3 = (sp3);
+//           }
+//         }
+//
+//         else if (sp2 >= 10) {
+//           slog2 = (':' + sp2);
+//           if (sp3 < 10) {
+//             slog3 = ('0' + sp3);
+//           }
+//           else if (sp3 >= 10) {
+//             slog3 = (sp3);
+//           }
+//         }
+//
+//         else if (sp2 >= 60) {
+//           sp2 = 0;
+//           sp3++;
+//           if (sp3 < 10) {
+//             slog3 = ('0' + sp3);
+//           }
+//           else if (sp3 >= 10) {
+//             slog3 = (sp3);
+//           }
+//         }
+//       }
+//
+//
+//       else if (sp1 >= 60) {
+//         sp1 = 0;
+//         sp2++;
+//         if (sp2 < 10) {
+//           slog2 = (':0' + sp2);
+//           if (sp3 < 10) {
+//             slog3 = ('0' + sp3);
+//           }
+//           else if (sp3 >= 10) {
+//             slog3 = (sp3);
+//           }
+//         }
+//
+//         else if (sp2 >= 10) {
+//           slog2 = (':' + sp2);
+//           if (sp3 < 10) {
+//             slog3 = ('0' + sp3);
+//           }
+//           else if (sp3 >= 10) {
+//             slog3 = (sp3);
+//           }
+//         }
+//
+//         else if (sp2 >= 60) {
+//           sp2 = 0;
+//           sp3++;
+//           if (sp3 < 10) {
+//             slog3 = ('0' + sp3);
+//           }
+//           else if (sp3 >= 10) {
+//             slog3 = (sp3);
+//           }
+//         }
+//       }
+//       document.querySelector('p1').innerHTML = (slog3 + slog2 + slog1);
+// }
